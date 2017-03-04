8348a1aa0b38ad3545fb15e9ec25aaeb1a709773
Zigmund Sun Oo <zigmundsunoo@gmail.com>
2017-03-03 20:09:08 -0600

    [master] safeguarding if list item does or does not exist for spinning

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -42,8 +42,14 @@ class ListComponent extends React.Component {
     this.getRandomValue();
   }
   getRandomValue(){
-    let randomValue = this.state.listItems[Math.floor(Math.random() * this.state.listItems.length)]['value'];
-    this.setState({randomValue}) 
+    if(this.state.listItems.length > 1){
+      let randomValue = this.state.listItems[Math.floor(Math.random() * this.state.listItems.length)]['value'];
+      this.setState({randomValue})   
+    }
+    else if(this.state.listItems.length == 1){
+      this.setState({randomValue: 'You only have one value. Please add more items...'})
+    }
+    else this.setState({randomValue: 'There are no values. Add items to the list.'});
   }
   componentWillMount(){
     // console.log('componentWillMount');
@@ -63,6 +69,7 @@ class ListComponent extends React.Component {
           <div className="row">
             <RandomItemSpinnerComponent randomValue={this.state.randomValue}/>
             <ListItemsComponent {...this.state} handleDeleteItem={this.handleDeleteItem.bind(this)}/>
+            <hr className="col-xs-12"/>
             <ListFormComponent  handleSubmitItem={this.handleSubmitItem.bind(this)} handleSpin={this.handleSpin.bind(this)}/>
           </div>
         </div>
