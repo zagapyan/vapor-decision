15c21572e735724301a86617bb8f921607d0189b
Zigmund Sun Oo <zigmundsunoo@gmail.com>
2018-03-14 20:50:48 -0500

    [master] conditional for login/logout

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,46 +1,69 @@
-'use strict';
+"use strict";
 
-import React from 'react';
-import { hashHistory, Link } from 'react-router';
+import React from "react";
+import { hashHistory, Link } from "react-router";
 // import { logout } from '../helpers/auth'
-import '../styles//Header.scss';
+import "../styles//Header.scss";
 
 class HeaderComponent extends React.Component {
-  constructor(props){
-  	super(props);
-    this.state={
-      alertClass: this.props.authed ? '' : 'text-center',
-      message: this.props.authed ? '' : 'You are currently not logged in.',
-      loginStyle: this.props.location.pathname == '/login' ? {display: 'none'} : {}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      alertClass: this.props.authed ? "" : "text-center",
+      message: this.props.authed ? "" : "You are currently not logged in.",
+      loginStyle:
+        this.props.location.pathname == "/login" ? { display: "none" } : {}
+    };
   }
-  onLogout(e){
+  onLogout(e) {
     e.preventDefault();
     this.props.handleLogout();
   }
-  componentWillMount(){
-
-  }
-  componentDidMount(){
-  }
+  componentWillMount() {}
+  componentDidMount() {}
   render() {
-  	const loginTrigger = this.props.authed ? <button onClick={this.onLogout.bind(this)} className="btn btn-danger logout-button">Log Out</button> : <Link to="login" className="btn btn-danger logout-button" style={this.state.loginStyle}>Login</Link>;
+    this.props.location.pathname !== "/login";
+    const loginTrigger =
+      this.props.location.pathname !== "login" ? (
+        this.props.authed ? (
+          <button
+            onClick={this.onLogout.bind(this)}
+            className="btn btn-danger logout-button"
+          >
+            Log Out
+          </button>
+        ) : (
+          <Link
+            to="login"
+            className="btn btn-danger logout-button"
+            style={this.state.loginStyle}
+          >
+            Login
+          </Link>
+        )
+      ) : (
+        ""
+      );
     return (
       <div className="header-component">
         <div className="container">
-        	<div className="col-xs-2">
-        	</div>
-        	<div className="col-xs-8">
-        		<h1 className="site-header text-center"><Link to="/">Vapor Decision</Link></h1></div>
-        	<div className="col-xs-2"><div className="row">{loginTrigger}</div></div>
-      	</div>
+          <div className="col-xs-2" />
+          <div className="col-xs-8">
+            <h1 className="site-header text-center">
+              <Link to="/">Vapor Decision</Link>
+            </h1>
+          </div>
+          <div className="col-xs-2">
+            <div className="row">{loginTrigger}</div>
+          </div>
+        </div>
         <p className={this.state.alertClass}>{this.state.message}</p>
       </div>
     );
   }
 }
 
-HeaderComponent.displayName = 'HeaderComponent';
+HeaderComponent.displayName = "HeaderComponent";
 
 // Uncomment properties you need
 // HeaderComponent.propTypes = {};
